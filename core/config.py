4bf9406c4d2e3f074d696aa8704c1f6d2beafa07
[QA3]이원기 <wonki0723@nate.com>
2026-04-24 22:02:27 +0900

    config.py update

--- core/config.py
+++ core/config.py
@@ -3,8 +3,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_KEY = os.getenv("TWELVELABS_API_KEY")
-INDEX_ID = os.getenv("TWELVELABS_INDEX_ID")
+#API_KEY = os.getenv("TWELVELABS_API_KEY")
+#INDEX_ID = os.getenv("TWELVELABS_INDEX_ID")
+
+# 시스템 환경 변수에서 직접 가져옵니다.
+API_KEY = os.environ.get("TWELVELABS_API_KEY")
+INDEX_ID = os.environ.get("TWELVELABS_INDEX_ID")
 
 if not API_KEY:
     raise ValueError("TWELVELABS_API_KEY is not set")
